539727a0c9d783cceeb6841ec8ee4dd58ebaba6e
bsautron <sautron.brunojj@gmail.com>
2015-02-20 07:12:55 +0100

    soulignement

--- ft_move.c
+++ ft_move.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/20 06:49:43 by bsautron          #+#    #+#             */
-/*   Updated: 2015/02/20 06:59:00 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/02/20 07:12:07 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,33 @@ void	ft_move(t_win win, char c)
 	static int	y;
 	char		*res;
 
+	res = tgetstr("cm", NULL);
 	if (c == 'A')
-		y--;
+	{
+		if (y > 0)
+		{
+			tputs(tgoto(res, x * (win.len_bigger + 1), y), 1, ft_outc);
+			ft_putstr("\033[0m");
+			ft_putstr(win.list->first->str);
+			y--;
+			win.list->first = win.list->first->prev;
+		}
+	}
 	else if (c == 'B')
+	{
+		tputs(tgoto(res, x * (win.len_bigger + 1), y), 1, ft_outc);
+		ft_putstr("\033[0m");
+		ft_putstr(win.list->first->str);
 		y++;
+		win.list->first = win.list->first->next;
+	}
 	else if (c == 'C')
 		x++;
 	else if (c == 'D')
 		x--;
 	if (x < 0)
 		x = 0;
-	res = tgetstr("cm", NULL);
 	tputs(tgoto(res, x * (win.len_bigger + 1), y), 1, ft_outc);
+	ft_putstr("\033[37;0;4m");
+	ft_putstr(win.list->first->str);
 }
--- ft_refresh.c
+++ ft_refresh.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/20 04:37:02 by bsautron          #+#    #+#             */
-/*   Updated: 2015/02/20 05:31:52 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/02/20 07:03:50 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ t_win	ft_refresh(void)
 
 	win = ft_get_info_for_win();
 	ft_tcg(0);
-	ft_make_instruction("ho", NULL);
+	//ft_make_instruction("ho", NULL);
+	ft_make_instruction("cl", NULL);
 	ft_print_argv(win);
 	return (win);
 }
