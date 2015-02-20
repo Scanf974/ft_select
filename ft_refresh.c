25b73e59660919a3f032229db7d100f3779c5759
bsautron <sautron.brunojj@gmail.com>
2015-02-20 04:55:45 +0100

    refres this

---
+++ ft_refresh.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_refresh.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/20 04:37:02 by bsautron          #+#    #+#             */
+/*   Updated: 2015/02/20 04:49:01 by bsautron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+
+void	ft_refresh(void)
+{
+	t_win	win;
+
+	win = ft_get_info_for_win();
+	ft_tcg(0);
+	ft_make_instruction("ho", NULL);
+	ft_print_argv(win);
+}
---
+++ ft_signal_handle.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_signal_handle.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/20 04:09:15 by bsautron          #+#    #+#             */
+/*   Updated: 2015/02/20 04:40:01 by bsautron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+struct termios	*g_dat;
+void	ft_signal_handler(int sig);
+
+static void        handle_sigtstp(void)
+{
+	char    cp[2] ={g_dat->c_cc[VSUSP], 0};
+	dprintf(1, "Stopping...\n");
+	ft_tcg(1);
+	signal(SIGTSTP, SIG_DFL);
+	ioctl(0, TIOCSTI, cp);
+}
+
+static void		handle_sigcont(void)
+{
+	ft_tcg(0);
+	signal(SIGTSTP, ft_signal_handler);
+}
+
+void	ft_signal_handler(int sig)
+{
+	if (sig == SIGWINCH)
+	{
+		ft_refresh();
+	}
+	if (sig == SIGTSTP)
+		handle_sigtstp();
+	if (sig == SIGCONT)
+		handle_sigcont();
+}
