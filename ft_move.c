35df7c333ca6af2cbd4a3071eaef70d49927ad22
bsautron <sautron.brunojj@gmail.com>
2015-02-22 13:54:30 +0100

    sofjsidfj

--- ft_move.c
+++ ft_move.c
@@ -6,7 +6,7 @@
 /*   By: bsautron <bsautron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/20 06:49:43 by bsautron          #+#    #+#             */
-/*   Updated: 2015/02/22 12:46:08 by bsautron         ###   ########.fr       */
+/*   Updated: 2015/02/22 13:53:28 by bsautron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,24 @@ void	ft_move(char c)
 		ft_make_instruction("se", NULL);
 		win->pos++;
 	}
-	else if (c == 'C')
-		x++;
+	else if (c == 'C' && win->pos + win->raw <= win->list->len)
+	{
+		tputs(tgoto(res, ((win->pos - 1) / win->height) * (win->len_bigger + 1), (win->pos - 1) % win->height), 1, ft_outc);
+		if (tmp->selected == 1)
+			ft_make_instruction("so", NULL);
+		ft_putstr(ft_get_link_by_id(win->pos - 1)->str);
+		ft_make_instruction("se", NULL);
+		win->pos += win->height;
+	}
 	else if (c == 'D')
-		x--;
+	{
+		tputs(tgoto(res, ((win->pos - 1) / win->height) * (win->len_bigger + 1), (win->pos - 1) % win->height), 1, ft_outc);
+		if (tmp->selected == 1)
+			ft_make_instruction("so", NULL);
+		ft_putstr(ft_get_link_by_id(win->pos - 1)->str);
+		ft_make_instruction("se", NULL);
+		win->pos -= win->height;
+	}
 	if (win->pos > win->nb_argv)
 		win->pos = 1;
 	if (win->pos == 0)
